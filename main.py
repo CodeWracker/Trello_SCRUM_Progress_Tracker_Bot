691c79de2470f753d4340b8c5edab9e0aaaba51c
CodeWracker <dev.rodrigofs@gmail.com>
2022-09-15 22:35:05 -0300

    fiz a geração das visualizações

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from definitions import *
 from trello_api import TRELLO_API
-from output_utils import generate_difficulty_chart,generate_crit_activity_relationship
+from output_utils import generate_difficulty_chart, generate_crit_activity_relationship
 
 from datetime import datetime
 from termcolor import colored
 
+
 def get_cards():
-    trello_api = TRELLO_API(TRELLO_BOARD_ID,IGNORE_LIST_IDS,IGNORE_CARDS_IDS, DONE_LIST_ID)
+    trello_api = TRELLO_API(
+        TRELLO_BOARD_ID, IGNORE_LIST_IDS, IGNORE_CARDS_IDS, DONE_LIST_ID)
     lists = (trello_api.get_board_lists())
     card_list_not_done = []
     card_list_done = []
@@ -16,35 +18,39 @@ def get_cards():
             print(list['name'])
             cards = trello_api.get_list_cards(list['id'])
             for card in cards:
-                if(card['id'] in trello_api.ignore_cards_ids):
+                if (card['id'] in trello_api.ignore_cards_ids):
                     continue
                 difficulty = 0
                 criterias = []
-                due =int( datetime.fromisoformat(str(card['due']).split("T")[0]).timestamp())
-                #print(due)
+                due = int(datetime.fromisoformat(
+                    str(card['due']).split("T")[0]).timestamp())
+                # print(due)
                 labels = card['labels']
                 for label in labels:
                     try:
                         difficulty = int(label['name'])
                     except:
                         criterias.append(label['name'])
-                all_cards.append([card['name'],difficulty,criterias,due])
+                all_cards.append([card['name'], difficulty, criterias, due])
                 if list['id'] == trello_api.done_list_id:
                     comments = trello_api.get_card_comments(card['id'])
                     for comment in comments:
                         txt = comment['data']['text'].split(":")
-                        if  txt[0] == "concluido":
-                            element = datetime.strptime(txt[1]," %Y-%m-%d")
+                        if txt[0] == "concluido":
+                            element = datetime.strptime(txt[1], " %Y-%m-%d")
                             timestamp = datetime.timestamp(element)
-                            card_list_done.append([card['name'],difficulty,criterias,int(timestamp)])
+                            card_list_done.append(
+                                [card['name'], difficulty, criterias, int(timestamp)])
                 else:
-                    card_list_not_done.append([card['name'],difficulty,criterias,due])
-    return all_cards,card_list_not_done, card_list_done
+                    card_list_not_done.append(
+                        [card['name'], difficulty, criterias, due])
+    return all_cards, card_list_not_done, card_list_done
 
 
 def sort_cards(card_list):
     card_list.sort(key=lambda x: x[3])
     return card_list
+
 
 def get_accumulated_difficulty(card_list_not_done, card_list_done):
     not_done_difficulty_acc = []
@@ -52,12 +58,13 @@ def get_accumulated_difficulty(card_list_not_done, card_list_done):
     acc = 0
     for card in card_list_not_done:
         acc = acc + card[1]
-        not_done_difficulty_acc.append([card[3],acc])
+        not_done_difficulty_acc.append([card[3], acc])
     acc = 0
     for card in card_list_done:
         acc = acc + card[1]
-        done_difficulty_acc.append([card[3],acc])
+        done_difficulty_acc.append([card[3], acc])
     return not_done_difficulty_acc, done_difficulty_acc
+
 
 def get_crit_activity_list(all_cards):
     crit_activity = {}
@@ -65,24 +72,27 @@ def get_crit_activity_list(all_cards):
         for crit in card[2]:
             if crit not in crit_activity:
                 crit_activity[crit] = []
-            crit_activity[crit].append(card[0]) 
+            crit_activity[crit].append(card[0])
     return crit_activity
+
+
 if __name__ == "__main__":
 
-    all_cards_list,card_list_not_done, card_list_done = get_cards()
+    all_cards_list, card_list_not_done, card_list_done = get_cards()
 
     card_list_not_done = sort_cards(card_list_not_done)
     card_list_done = sort_cards(card_list_done)
     all_cards_list = sort_cards(all_cards_list)
-    
-    all_cards_difficulty_acc, done_difficulty_acc = get_accumulated_difficulty(all_cards_list, card_list_done)
-    
+
+    all_cards_difficulty_acc, done_difficulty_acc = get_accumulated_difficulty(
+        all_cards_list, card_list_done)
+
     crit_activity = get_crit_activity_list(all_cards_list)
 
     generate_difficulty_chart(all_cards_difficulty_acc, done_difficulty_acc)
     generate_crit_activity_relationship(crit_activity)
-    
-    for crit in crit_activity:
+
+    '''for crit in crit_activity:
         print(colored(f'{crit} : ',"white"),colored(f'{crit_activity[crit]}',"yellow"))
     print()
 
@@ -94,4 +104,4 @@ if __name__ == "__main__":
     for acc in done_difficulty_acc:
         dt = datetime.fromtimestamp(acc[0])
         str_dt = dt.strftime("%d/%m/%Y")
-        print(f'{str_dt } : {acc[1]}')
+        print(f'{str_dt } : {acc[1]}')'''
--- output_utils.py
+++ output_utils.py
@@ -1,7 +1,92 @@
-# Escreve tuas coisas aqui @Emir
+from datetime import datetime
+from heapq import merge
+import plotly.graph_objects as go
+import xlsxwriter
+
 
 def generate_difficulty_chart(all_cards_difficulty_acc, done_difficulty_acc):
-    return
+
+    # Generate the difficulty chart using plotly and save it as a png file
+    # The chart is a line chart with two lines, one for the accumulated difficulty of all cards and the other for the accumulated difficulty of done cards
+
+    # Convert the timestamps to dates
+    all_cards_dates = []
+    done_dates = []
+    for acc in all_cards_difficulty_acc:
+        dt = datetime.fromtimestamp(acc[0])
+        all_cards_dates.append(dt)
+    for acc in done_difficulty_acc:
+        dt = datetime.fromtimestamp(acc[0])
+        done_dates.append(dt)
+
+    # Create the chart
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=all_cards_dates, y=[
+                  acc[1] for acc in all_cards_difficulty_acc], name="Todas as Tarefas"))
+    fig.add_trace(go.Scatter(x=done_dates, y=[
+                  acc[1] for acc in done_difficulty_acc], name="Feito"))
+    fig.update_layout(
+        title="Acompanhamento do Progresso",
+        xaxis_title="Data",
+        yaxis_title="Dificuldade Acumulada",
+        legend_title="Legenda",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="RebeccaPurple"
+        )
+    )
+
+    # Save the chart as a png file with todays date
+    today = datetime.today()
+    str_today = today.strftime("%Y-%m-%d")
+    # change fig size
+    fig.update_layout(
+        autosize=False,
+        width=1000,
+        height=500,
+
+
+    )
+
+    fig.write_image(f"output/{str_today}-difficulty_chart.png")
+
 
 def generate_crit_activity_relationship(crit_activity):
+    # generate a xlsx file with the relationship between the criteria and the cards that are related to them
+    # the file is saved in the output folder
+
+    # Create a workbook and add a worksheet.
+    workbook = xlsxwriter.Workbook('output/crit_activity_relationship.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    # Start from the first cell. Rows and columns are zero indexed.
+
+    worksheet.write(0, 0, "Critérios")
+    worksheet.write(0,  1, "Atividades")
+    row = 1
+    col = 0
+
+    crits = []
+    for crit in crit_activity:
+        crits.append(crit)
+    # sort crits
+    crits.sort()
+
+    # Iterate over the data and write it out row by row.
+    for crit in crits:
+        worksheet.write(row, col, crit)
+        row_ini = row
+        row_end = row
+        for activity in crit_activity[crit]:
+            worksheet.write(row, col, crit)
+            worksheet.write(row, col + 1, activity)
+            row_end = row
+            row += 1
+        # merge cells that are the same
+        if (row_ini != row_end):
+            worksheet.merge_range(row_ini, col, row_end, col, crit)
+
+    workbook.close()
+
     return
